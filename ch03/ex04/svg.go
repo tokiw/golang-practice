9e60a515286bda5ae7f0a040f2186d04754adcad
tokiw <masafumi@tokiwa-MacBook-Pro.local>
2017-05-22 22:49:49 +0900

    add ch03/ex01-04

---
+++ ch03/ex04/svg.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func svg() string {
+	var sgv string
+	sgv += fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			if !validate([]float64{ax, ay, bx, by, cx, cy, dx, dy}) {
+				continue
+			}
+			sgv += fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color(i, j))
+		}
+	}
+	sgv += fmt.Sprintln("</svg>")
+	return sgv
+}
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
+
+func validate(values []float64) bool {
+	for _, value := range values {
+		return !math.IsNaN(value) && !math.IsInf(value, 0)
+	}
+	return true
+}
+
+func color(i, j int) string {
+	const min = 0.3
+	const max = 1.0
+	z := f(xyrange*(float64(i)/cells-0.5), xyrange*(float64(j)/cells-0.5))
+
+	r := (z + min) / (min + max) * 256
+
+	return fmt.Sprintf("#%02x00%02x", int(r), int(255-r))
+}
