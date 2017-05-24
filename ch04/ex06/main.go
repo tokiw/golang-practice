5b5fbe72eb147d6900ee7a54a210c9f4e530addf
tokiw <masafumi@tokiwa-MacBook-Pro.local>
2017-05-25 01:20:01 +0900

    add ch04/ex06, 09

---
+++ ch04/ex06/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	str1 := "aa bb"
+	fmt.Println(string(replaceSpace([]byte(str1))))
+	str2 := "あ　い"
+	fmt.Println(string(replaceSpace([]byte(str2))))
+}
+
+func replaceSpace(s []byte) []byte {
+	runes := bytes.Runes(s)
+
+	for i := 0; i < len(runes); i++ {
+		if unicode.IsSpace(runes[i]) {
+			runes[i] = ' '
+		}
+	}
+	return []byte(string(runes))
+}
---
+++ ch04/ex09/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != err {
+		fmt.Fprintln(os.Stderr, "Failed file open")
+	}
+	input := bufio.NewScanner(file)
+	input.Split(bufio.ScanWords)
+
+	words := make(map[string]int)
+
+	for input.Scan() {
+		word := input.Text()
+		words[word]++
+	}
+
+	for word, count := range words {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
