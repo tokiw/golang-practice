7cadfc22f08eae72775fe488c40606f254f53e9e
tokiw <masafumi@tokiwa-MacBook-Pro.local>
2017-07-27 01:07:51 +0900

    add ch08/ex05

---
+++ ch08/ex05/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func BenchmarkDrawParallel2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		drawParallel(&buf, 2)
+	}
+}
+
+func BenchmarkDrawParallel3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		drawParallel(&buf, 3)
+	}
+}
+
+func BenchmarkDrawParallel4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		drawParallel(&buf, 4)
+	}
+}
+
+func BenchmarkDrawParallel5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		drawParallel(&buf, 5)
+	}
+}
+
+func BenchmarkDrawParallel10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		drawParallel(&buf, 10)
+	}
+}
+
+// func BenchmarkDrawParallel50(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		var buf bytes.Buffer
+// 		drawParallel(&buf, 50)
+// 	}
+// }
